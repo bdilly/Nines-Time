6dad95a34c3a7a7ea0fd15c066159f7ade1bc19a
Bruno Dilly <bdilly@profusion.mobi>
2011-04-06 20:30:05 -0300

    Add Linux Libertine font to our theme
    Adding a font is a good idea so we'll be sure the game will
    have the same look at any platform.

--- build_theme.sh
+++ build_theme.sh
@@ -3,6 +3,6 @@
 DATADIR="`dirname $0`/data/theme"
 
 EDJE_CC=edje_cc
-EDJE_FLAGS="-v -id $DATADIR/images"
+EDJE_FLAGS="-v -id $DATADIR/images -fd $DATADIR/fonts"
 
 $EDJE_CC $EDJE_FLAGS $DATADIR/default.edc $DATADIR/default.edj
